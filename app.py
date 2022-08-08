f5aa2313a09630ec20c0f8931b40773cb7de2d78
manthanp1012 <manthanpatil912@gmail.com>
2022-08-09 03:33:16 +0530

    fourth Commit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import jsonify
 import requests
 import pickle
-#import numpy as np
+import numpy as np
 import sklearn
 from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
